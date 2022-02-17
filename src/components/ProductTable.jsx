46e77ecfa20c3bf34471fa29765add6dc3fcc0bc
Alicia Kmak <alicia.kmak@gmail.com>
2022-02-17 20:23:39 +0100

    iteration 0 and 1 done

---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,14 @@
+import ProductRow from './ProductRow'
+
+function ProductTable() {
+  
+
+  return (
+    <div>
+      <h1>IronProducts</h1>
+      <ProductRow />
+    </div>
+  );
+}
+
+export default ProductTable
